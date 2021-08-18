49933606a520a24e68ba3da713c3bd24db30e482
Sanmuell <sanmuell.i@gmail.com>
2021-08-17 21:56:54 -0300

    Adicionando o formulário de pesquisa de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccordionModule } from 'primeng/accordion'; // accordion and accordion tab          // api
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    InputTextModule,
+
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
