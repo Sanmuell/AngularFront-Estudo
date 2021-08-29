64f1820ba64cd603a155f058f087251759b241a5
Sanmuell <sanmuell.i@gmail.com>
2021-08-29 18:59:28 -0300

    Adicionando seletor de data (componente Calendar)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule} from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccordionModule } from 'primeng/accordion'; // accordion and accordion tab          // api
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { AccordionModule } from 'primeng/accordion'; 
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
 import {TooltipModule} from 'primeng/tooltip';
 import { ButtonModule } from 'primeng/button';
 import {InputTextareaModule} from 'primeng/inputtextarea';
 
+
+import {CalendarModule} from 'primeng/calendar';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
@@ -25,8 +30,11 @@ import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-ca
     NavbarComponent,
     PessoasPesquisaComponent,
     LancamentoCadastroComponent
+
   ],
+
   imports: [
+
     BrowserModule,
     AppRoutingModule,
     AccordionModule,
@@ -34,7 +42,9 @@ import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-ca
     ButtonModule,
     InputTextModule,
     TooltipModule,
-    InputTextareaModule
+    InputTextareaModule,
+    CalendarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
