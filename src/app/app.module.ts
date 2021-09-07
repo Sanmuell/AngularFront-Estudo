48cdc04c7c975b728edbd0e4c760a0ff0c5a636e
Sanmuell <sanmuell.i@gmail.com>
2021-09-07 19:10:55 -0300

    Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
+import { CoreModule } from './core/core.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasModule } from './pessoas/pessoas.module';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
 
   ],
 
@@ -20,7 +18,8 @@ import { PessoasModule } from './pessoas/pessoas.module';
   BrowserModule,
   BrowserAnimationsModule,
   LancamentosModule,
-  PessoasModule
+  PessoasModule,
+  CoreModule
 
   ],
   providers: [],
